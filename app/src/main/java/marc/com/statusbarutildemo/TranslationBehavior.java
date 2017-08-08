2ccb0e7651bcba734a09e83bde13076214374b81
marc <wangchengda1990@gmail.com>
2017-08-08 12:25:30 +0800

    自定义behavior

---
+++ app/src/main/java/marc/com/statusbarutildemo/TranslationBehavior.java
@@ -0,0 +1,61 @@
+package marc.com.statusbarutildemo;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by 王成达
+ * Date: 2017/8/8
+ * Time: 09:42
+ * Version: 1.0
+ * Description:
+ * Email:wangchengda1990@gmail.com
+ **/
+public class TranslationBehavior extends FloatingActionButton.Behavior {
+
+	private boolean mIsOut = false;
+
+	public TranslationBehavior(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	/**
+	 * 关注垂直滚动，向上出来，向下消失隐藏
+	 * @param coordinatorLayout
+	 * @param child
+	 * @param target
+	 * @param dxConsumed
+	 * @param dyConsumed
+	 * @param dxUnconsumed
+	 * @param dyUnconsumed
+	 */
+	@Override
+	public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+		Log.d("TAG", "onNestedScroll: dyConsumed->"+dyConsumed);
+		if(dyConsumed > 0){
+			//向下
+			if(!mIsOut ) {
+				int translationY = ((CoordinatorLayout.LayoutParams) child.getLayoutParams()).bottomMargin + child.getMeasuredHeight();
+				child.animate().translationY(translationY).setDuration(500).start();
+				mIsOut = true;
+			}
+		}else{
+//			向上
+			if(mIsOut) {
+				child.animate().translationY(0).setDuration(500).start();
+				mIsOut = false;
+			}
+		}
+	}
+
+
+	@Override
+	public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+		return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+	}
+}
